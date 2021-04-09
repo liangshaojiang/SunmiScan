f50ac3f627350e3395903abea726f68349202834
liangshaojiang <920999181@qq.com>
2021-04-09 16:50:52 +0800

    添加项目文件。

---
+++ SunmiScan/SunmiScan/App.xaml.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using Xamarin.Forms;
+using Xamarin.Forms.Xaml;
+
+namespace SunmiScan
+{
+    public partial class App : Application
+    {
+        public App()
+        {
+            InitializeComponent();
+
+            MainPage = new ScanQrCodePage();
+        }
+
+        protected override void OnStart()
+        {
+        }
+
+        protected override void OnSleep()
+        {
+        }
+
+        protected override void OnResume()
+        {
+        }
+    }
+}
---
+++ SunmiScan/SunmiScan/MainPage.xaml.cs
@@ -0,0 +1,42 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Xamarin.Forms;
+
+namespace SunmiScan
+{
+    public partial class MainPage : ContentPage
+    {
+        public MainPage()
+        {
+            InitializeComponent();
+
+            btn_scan.Clicked += async (sender, e) =>
+            {
+
+#if __ANDROID__
+	// Initialize the scanner first so it can track the current context
+	MobileBarcodeScanner.Initialize (Application);
+#endif
+
+                var scanner = new ZXing.Mobile.MobileBarcodeScanner();
+
+                var result = await scanner.Scan(ZXing.Mobile.MobileBarcodeScanningOptions.Default);
+
+                if (result != null) {
+                    Console.WriteLine("Scanned Barcode: " + result.Text);
+                    lab_text.Text = result.Text;
+                   // Toast.MakeText(this, "" + singleCheck, ToastLength.Long).Show();
+                }
+                  
+            };
+
+
+
+
+        }
+    }
+}
